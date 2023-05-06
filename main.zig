f3efdcaa03abd49a3342d49fa938a62691705785
Airbus5717 <55999379+Airbus5717@users.noreply.github.com>
2023-05-06 18:48:50 +0300

    annotate instructions for readability

--- main.zig
+++ main.zig
@@ -11,34 +11,34 @@ const CU = packed struct(u8) {
 
 const opControl = [_]CU{
     // R-type (opcodes from 0 to 2 are 3 opcodes only)
-    .{ .BusW = 0, .RegDest = 0, .AluB = 0, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 0, .AluB = 0, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 0, .AluB = 0, .PCSrc = 0 },
+    .{ .BusW = 0, .RegDest = 0, .AluB = 0, .PCSrc = 0 }, // ADD ... SLTU
+    .{ .BusW = 0, .RegDest = 0, .AluB = 0, .PCSrc = 0 }, // XOR ... NOR
+    .{ .BusW = 0, .RegDest = 0, .AluB = 0, .PCSrc = 0 }, // SLL ... ROR
     // I-Type
-    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 },
+    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 0 }, // ADDI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 0 }, // SLTI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 0 }, // SLTIU
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // XORI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // ORI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // ANDI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // NORI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // SLLI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // SRLI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // SRAI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 2, .PCSrc = 0 }, // RORI
     // load & store
-    .{ .BusW = 1, .RegDest = 1, .AluB = 1, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 1 },
+    .{ .BusW = 1, .RegDest = 1, .AluB = 1, .PCSrc = 0 }, // LW
+    .{ .BusW = 0, .RegDest = 1, .AluB = 1, .PCSrc = 1 }, // SW
     // branch
-    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 1 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 1 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 1 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 0 },
+    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 1 }, // BEQ
+    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 1 }, // BNE
+    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 1 }, // BLT
+    .{ .BusW = 0, .RegDest = 1, .AluB = 0, .PCSrc = 0 }, // BGE
     // jump, lui
-    .{ .BusW = 2, .RegDest = 1, .AluB = 1, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 2, .AluB = 3, .PCSrc = 0 },
-    .{ .BusW = 0, .RegDest = 1, .AluB = 3, .PCSrc = 2 },
-    .{ .BusW = 0, .RegDest = 3, .AluB = 3, .PCSrc = 2 },
+    .{ .BusW = 2, .RegDest = 1, .AluB = 1, .PCSrc = 0 }, // JALR
+    .{ .BusW = 0, .RegDest = 2, .AluB = 3, .PCSrc = 0 }, // LUI
+    .{ .BusW = 0, .RegDest = 1, .AluB = 3, .PCSrc = 2 }, // J
+    .{ .BusW = 0, .RegDest = 3, .AluB = 3, .PCSrc = 2 }, // JAL
 };
 
 pub fn main() !void {
